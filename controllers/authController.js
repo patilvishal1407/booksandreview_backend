8cbc1ed57799c79fb6caade3fdf573d2a4e4db1f
patilvishal1407 <patilvishalvenkatrao@gmail.com>
2025-06-05 18:41:26 +0530

    authenticationaccrss

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,12 +15,13 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await getUserByEmail(email);
+  // console.log("user", user)
   if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
   const match = await bcrypt.compare(password, user.password);
   if (!match) return res.status(400).json({ message: 'Invalid credentials' });
 
-  const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET);
+  const token = jwt.sign({ id: user.id, email: user.email,role:user.role }, process.env.JWT_SECRET);
   res.json({ token });
 };
 
